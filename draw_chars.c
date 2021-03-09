4051abb4aac8c8e40b6a1929dff7efc05dc9e403
vcabral001 <77982111+vcabral001@users.noreply.github.com>
2021-03-09 11:55:40 -0700

    Update draw_chars.c

--- draw_chars.c
+++ draw_chars.c
@@ -13,3 +13,18 @@ void print_char_11x16(char c)
     putchar('\n');
   }
 }
+
+void print_char_8x12(char c)
+{
+  c -= 0x20;
+  for (char row = 0; row < 8; row++) {
+    unsigned short rowBits = font_8x12[c][row];
+    for (char col = 0; col < 12; col++) {
+      unsigned short colMask = 1 << (15-col); /* mask to select bit associated with bit */
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+}
+
+
